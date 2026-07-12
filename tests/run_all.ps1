eaff6826092c9b573b181bae225066f90b3eb24a
sirius <95621941@qq.com>
2026-07-12 16:53:21 +0800

    test: run physics interaction regression

--- tests/run_all.ps1
+++ tests/run_all.ps1
@@ -37,6 +37,7 @@ Invoke-GodotTest 'res://tests/developer_b/run_tests.gd'
 Invoke-GodotTest 'res://tests/qa/integration_regression.gd'
 Invoke-GodotTest 'res://tests/qa/input_interaction_regression.gd'
 Invoke-GodotTest 'res://tests/qa/movement_lifecycle_regression.gd'
+Invoke-GodotTest 'res://tests/qa/physics_interaction_regression.gd'
 Invoke-GodotTest 'res://tests/qa/settings_retest.gd'
 
 Write-Host 'PASS: data registry + Godot runtime checks'
