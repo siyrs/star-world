588014d6b18223e275ae2dfb675dcf44f4ae7c02
sirius <95621941@qq.com>
2026-07-17 21:46:12 +0800

    test: add ecology danger to full local regression suite

--- tests/run_all.ps1
+++ tests/run_all.ps1
@@ -26,6 +26,7 @@ if ([string]::IsNullOrWhiteSpace($Godot) -or -not (Test-Path -LiteralPath $Godot
 & "$PSScriptRoot\developer_b\validate_catalog_integrity.ps1"
 & "$PSScriptRoot\developer_b\validate_resource_distribution.ps1"
 & "$PSScriptRoot\developer_b\validate_prospecting.ps1"
+& "$PSScriptRoot\developer_b\validate_ecology_danger.ps1"
 & "$PSScriptRoot\developer_b\validate_block_visuals.ps1"
 & "$PSScriptRoot\developer_b\validate_fertilizers.ps1"
 & "$PSScriptRoot\developer_b\validate_rest.ps1"
@@ -50,6 +51,7 @@ Invoke-GodotTest 'res://tests/qa/physics_interaction_regression.gd'
 Invoke-GodotTest 'res://tests/qa/block_interaction_regression.gd'
 Invoke-GodotTest 'res://tests/qa/resource_distribution_regression.gd'
 Invoke-GodotTest 'res://tests/qa/prospecting_regression.gd'
+Invoke-GodotTest 'res://tests/qa/ecology_danger_regression.gd'
 Invoke-GodotTest 'res://tests/qa/glass_pane_regression.gd'
 Invoke-GodotTest 'res://tests/qa/block_texture_regression.gd'
 Invoke-GodotTest 'res://tests/qa/non_cube_block_geometry_regression.gd'
@@ -80,4 +82,4 @@ Invoke-GodotTest 'res://tests/qa/runtime_stability_regression.gd'
 Invoke-GodotTest 'res://tests/qa/runtime_soak_regression.gd'
 Invoke-GodotTest 'res://tests/qa/settings_retest.gd'
 
-Write-Host 'PASS: catalog + resources + prospecting + panes + directional textured geometry + first-person + runtime checks'
+Write-Host 'PASS: catalog + resources + prospecting + ecology danger + panes + directional geometry + runtime checks'
