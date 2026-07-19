e5e47c959e86787c9f1aaac45003f76d51f091eb
sirius <95621941@qq.com>
2026-07-19 22:39:07 +0800

    refactor: reduce exploration hub to participant registration

--- src/ui/exploration_progression_service_hub.gd
+++ src/ui/exploration_progression_service_hub.gd
@@ -1,9 +1,6 @@
 class_name ExplorationProgressionServiceHub
 extends "res://src/ui/ranch_progression_service_hub.gd"
 
-const FeatureCoordinatorScript = preload(
-	"res://src/core/service_hub_feature_coordinator.gd"
-)
 const ExplorationRuntimeParticipantScript = preload(
 	"res://src/exploration/exploration_runtime_participant.gd"
 )
@@ -17,17 +14,12 @@ var prospecting_service: Node
 var exploration_danger_service: Node
 var exploration_journal_service: Node
 var exploration_reward_service: Node
-var feature_lifecycle: Node
 var exploration_runtime_participant: Node
 var exploration_journal_reward_participant: Node
 
 
 func _ready() -> void:
 	super._ready()
-	feature_lifecycle = _add_service(
-		FeatureCoordinatorScript.new(), "FeatureLifecycle"
-	)
-	feature_lifecycle.call("setup", self)
 	exploration_runtime_participant = _register_feature_participant(
 		EXPLORATION_RUNTIME_FEATURE,
 		ExplorationRuntimeParticipantScript.new(),
@@ -54,60 +46,6 @@ func _ready() -> void:
 		) as Node
 
 
-func _begin_world(state: Dictionary) -> void:
-	var migrated_state := state.duplicate(true)
-	if (
-		exploration_runtime_participant != null
-		and exploration_runtime_participant.has_method("normalize_world_state")
-	):
-		var raw_migrated: Variant = exploration_runtime_participant.call(
-			"normalize_world_state", state
-		)
-		if raw_migrated is Dictionary:
-			migrated_state = raw_migrated
-	if feature_lifecycle != null:
-		feature_lifecycle.call("begin_world", migrated_state)
-	super._begin_world(migrated_state)
-
-
-func attach_game(
-	world,
-	player: Node3D,
-	sun: DirectionalLight3D = null,
-	environment: WorldEnvironment = null,
-	ground_resolver: Callable = Callable()
-) -> void:
-	super.attach_game(world, player, sun, environment, ground_resolver)
-	if feature_lifecycle != null:
-		feature_lifecycle.call(
-			"attach_game", world, player, sun, environment, ground_resolver
-		)
-
-
-func activate_gameplay() -> void:
-	super.activate_gameplay()
-	if feature_lifecycle != null:
-		feature_lifecycle.call("activate")
-
-
-func save_current(world_state: Dictionary = {}, player_state: Dictionary = {}) -> bool:
-	if feature_lifecycle != null:
-		feature_lifecycle.call("save_into", current_state)
-	return super.save_current(world_state, player_state)
-
-
-func handle_world_start_failed(reason: String) -> void:
-	if feature_lifecycle != null:
-		feature_lifecycle.call("clear", &"world_start_failed")
-	super.handle_world_start_failed(reason)
-
-
-func return_to_menu() -> void:
-	super.return_to_menu()
-	if current_world_id.is_empty() and feature_lifecycle != null:
-		feature_lifecycle.call("clear", &"return_to_menu")
-
-
 func get_character_snapshot() -> Dictionary:
 	var snapshot: Dictionary = super.get_character_snapshot()
 	snapshot["ecology"] = (
@@ -115,30 +53,4 @@ func get_character_snapshot() -> Dictionary:
 		if creature_spawner != null and creature_spawner.has_method("get_ecology_snapshot")
 		else {}
 	)
-	if feature_lifecycle != null:
-		feature_lifecycle.call("snapshot_into", snapshot)
-		snapshot["feature_lifecycle"] = feature_lifecycle.call("get_snapshot")
 	return snapshot
-
-
-func _exit_tree() -> void:
-	if feature_lifecycle != null and feature_lifecycle.has_method("shutdown"):
-		feature_lifecycle.call("shutdown")
-	super._exit_tree()
-
-
-func _register_feature_participant(
-	participant_id: StringName, participant: Node, label: String
-) -> Node:
-	if feature_lifecycle == null:
-		return null
-	var registration: Dictionary = feature_lifecycle.call(
-		"register_participant", participant_id, participant
-	)
-	if bool(registration.get("success", false)):
-		return registration.get("participant") as Node
-	push_error(
-		"Unable to install %s lifecycle participant: %s"
-		% [label, str(registration.get("reason", "unknown"))]
-	)
-	return null
