b83517ca0fe02bff9f90373b739c3aa704b8ead1
sirius <95621941@qq.com>
2026-07-27 07:38:08 +0800

    refactor(ui): adopt responsive professional subpanel sizing

--- src/ui/protected_main_menu.gd
+++ src/ui/protected_main_menu.gd
@@ -17,27 +17,30 @@ const ProtectedUpdatePromptPanelScript = preload(
 
 func _build_subpanels() -> void:
 	_map_panel = ProtectedMapPanelScript.new()
-	_center_panel(_map_panel, Vector2(860, 610))
+	_center_panel(_map_panel, Vector2(960, 610))
 	add_child(_map_panel)
 	_map_panel.visible = false
 	_map_panel.create_requested.connect(_on_create_requested)
 	_map_panel.back_requested.connect(show_main)
+
 	_save_panel = ProtectedSaveBrowserScript.new()
-	_center_panel(_save_panel, Vector2(820, 590))
+	_center_panel(_save_panel, Vector2(960, 610))
 	add_child(_save_panel)
 	_save_panel.visible = false
 	_save_panel.load_requested.connect(_on_load_requested)
 	_save_panel.back_requested.connect(show_main)
+
 	_settings_panel = ProtectedSettingsPanelScript.new()
-	_center_panel(_settings_panel, Vector2(700, 560))
+	_center_panel(_settings_panel, Vector2(780, 570))
 	add_child(_settings_panel)
 	_settings_panel.visible = false
 	_settings_panel.settings_applied.connect(
 		func(settings: Dictionary) -> void: settings_changed.emit(settings)
 	)
 	_settings_panel.back_requested.connect(show_main)
+
 	_update_panel = ProtectedUpdatePromptPanelScript.new()
-	_center_panel(_update_panel, Vector2(680, 500))
+	_center_panel(_update_panel, Vector2(760, 540))
 	add_child(_update_panel)
 	_update_panel.visible = false
 	_update_panel.dismissed.connect(show_main)
