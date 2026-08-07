574ab7f6b49aba957c0973c823f43000f8105483
sirius <95621941@qq.com>
2026-08-07 13:56:39 +0800

    feat: define signed updater protocol

--- src/update/app_version.gd
+++ src/update/app_version.gd
@@ -8,7 +8,8 @@ const PACKAGE_ASSET_NAME := "StarWorld-Windows-x86_64.zip"
 const CHECKSUM_ASSET_NAME := "StarWorld-Windows-x86_64.zip.sha256"
 const EXECUTABLE_NAME := "StarWorld.exe"
 const UPDATE_MANIFEST_NAME := "update-manifest.json"
-const UPDATER_PROTOCOL_VERSION := 1
+const UPDATE_MANIFEST_SIGNATURE_NAME := "update-manifest.p7s"
+const UPDATER_PROTOCOL_VERSION := 2
 
 
 static func display_version() -> String:
